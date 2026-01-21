3bb880312c1cfd5994018c57a79ae1f5337778a1
lamnp024 <dba.phuclam024@gmail.com>
2026-01-21 17:22:24 +0700

    initial project

---
+++ app/pages/project/[id].vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Hiển thị chi tiết theo id của project</h1>
+  </div>
+</template>
---
+++ app/pages/project/list.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Danh sách Project</h1>
+  </div>
+</template>
